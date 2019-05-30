87aed851ed911a64dd2234189f4a7f04937deafd
MrNikkiHD <chatchawan79604@gmail.com>
2019-05-30 11:38:03 +0700

    [ADD]New Project
    Computer Programming
    -flowchart
    -if else
    -int

---
+++ WIP-XI/src/III/Output/_1_Println.java
@@ -0,0 +1,22 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package III.Output;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class _1_Println {
+    public static void main(String[] args) {
+        String name = "วิป";
+        System.out.println("หวัดดีจั๊บ ผมชื่อ " + name);
+        
+        System.out.println("ถ้าเราเอา " + 1 + " ไปบวก " + 100 + " เราจะได้ " + 1+100);
+        String names = "แอล";
+        System.out.println("Hello ผมชื่อ" + names);
+        
+       
+    }
+}
